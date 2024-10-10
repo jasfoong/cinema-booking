74c7454a6e3ced10fe001aebfe9f56823556e5fd
Jasmine Foong <jasfoong19@gmail.com>
2024-10-10 14:50:24 -0700

    create views and define URLs for movie list page and movie details page

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.movie_list, name='movie_list'),
+    path('movies/<int:movie_id>/', views.movie_details, name='movie_details')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Movie, Showtimes
 
-# Create your views here.
-def home(request):
-    return HttpResponse("Welcome to the movie app!")
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies/movie_list.html', {'movies': movies})
+
+def movie_details(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    showtimes = Showtimes.objects.filter(movie=movie)
+    return render(request, 'movies/movie_details.html', {'movie': movie, 'showtimes': showtimes})
