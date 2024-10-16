1d89b4f15643a76af972b09445afec2401bc3215
Jasmine Foong <jasfoong19@gmail.com>
2024-10-16 14:04:17 -0700

    Add search and dropdown filters on movie_list html and use ajax to render filtered content dynamically

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Movie, Showtimes
+from django.http import HttpResponse
+from .models import Movie, Showtimes, Genre
 
 def movie_list(request):
+    search_query = request.GET.get('search', '')
+    selected_genre = request.GET.get('genre', '')
+
     movies = Movie.objects.all()
-    return render(request, 'movies/movie_list.html', {'movies': movies})
+
+    # Filter movies based on search query and selected genre
+    if search_query:
+        movies = movies.filter(title__icontains=search_query)
+    if selected_genre:
+        movies = movies.filter(genre__id=selected_genre)
+
+    genres = Genre.objects.all()
+
+    context = {
+        'movies': movies,
+        'genres': genres,
+        'search_query': search_query,
+        'selected_genre': selected_genre
+    }
+
+    # Check if request is AJAX
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return render(request, 'movies/movie_list_partial.html', context)
+
+    # If request is not AJAX, render the full template
+    return render(request, 'movies/movie_list.html', context)
 
 def movie_details(request, movie_id):
     movie = get_object_or_404(Movie, id=movie_id)
